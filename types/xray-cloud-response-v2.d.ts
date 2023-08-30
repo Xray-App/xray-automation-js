ca849bcce2540a0dfe0e8996637a83f64bbd4ae5
Sergio Freire <sergio.freire@gmail.com>
2023-08-30 10:32:54 +0100

    add types to the correct output folder

--- types/xray-cloud-response-v2.d.ts
+++ types/xray-cloud-response-v2.d.ts
@@ -1,8 +1,8 @@
 export declare class XrayCloudResponseV2 {
-  id?: string;
-  key?: string;
-  _response: string;
-  selfUrl?: string;
-  constructor(response: any);
+    id?: string;
+    key?: string;
+    _response: string;
+    selfUrl?: string;
+    constructor(response: any);
 }
 export default XrayCloudResponseV2;
